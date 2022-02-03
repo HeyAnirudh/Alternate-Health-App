b3d34db7d8cd7b02aecef20b8698a2847a215f28
Rishabh Agrawal <51956671+Rishabh241018@users.noreply.github.com>
2022-02-03 20:22:54 +0530

    dashboard

--- AHS/src/app/app.module.ts
+++ AHS/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { Mainpage1Component } from './mainpage1/mainpage1.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './mainpage/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainpageComponent,
     Mainpage1Component,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
