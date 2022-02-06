5a9fbaea285d83005ed20b585efae0eaa0da72c6
Anirudh soni <anirudh90hyd@gmail.com>
2022-02-06 22:30:03 +0530

    routing

--- AHS/src/app/app.module.ts
+++ AHS/src/app/app.module.ts
@@ -8,6 +8,15 @@ import { MainpageComponent } from './mainpage/mainpage.component';
 import { Mainpage1Component } from './mainpage1/mainpage1.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './mainpage/dashboard/dashboard.component';
+import { ProfileComponent } from './mainpage/profile/profile.component';
+import { AppointmentComponent } from './mainpage/appointment/appointment.component';
+import {Routes ,RouterModule} from '@angular/router';
+const appRoutes: Routes =[
+{path :'', component: DashboardComponent},
+{path :'profile', component: ProfileComponent},
+{path :'Appointment', component: AppointmentComponent},
+
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +24,16 @@ import { DashboardComponent } from './mainpage/dashboard/dashboard.component';
     MainpageComponent,
     Mainpage1Component,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProfileComponent,
+    AppointmentComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
 
 
   ],
